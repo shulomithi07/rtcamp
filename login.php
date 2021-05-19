f9108da999e69b859a2e5da4cfa8864ed16789a1
shulomithi07 <62322842+shulomithi07@users.noreply.github.com>
2021-05-20 01:38:21 +0530

    few changes for styling

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 
 session_start();
 
+
 include 'connection.php';
-include 'scripts/script.php';
-include 'css/style.php';
+include 'css/loginCSS.php';
 ?>
 
 
@@ -15,120 +15,146 @@ include 'css/style.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
+
 </head>
 <body>
-
-    <?php
     
+    <div class="signup">
 
-
-        if(isset($_POST['submit'])){
-
-            $email= $_POST['email'];
-            $CheckPassword = $_POST['password'];
-
-            $emailQuery = "SELECT * FROM members WHERE email ='$email'and status='active'";
-            // SELECT * FROM `members` WHERE email='shiny';
-            
-
-            $selectQuery = mysqli_query($con,$emailQuery);
-
-            $emailCount = mysqli_num_rows($selectQuery);
-
-            echo $emailCount;
-
-            if($emailCount>0){
-
-                $emailPassword = mysqli_fetch_assoc($selectQuery);
-
-                $loginPassword = $emailPassword['password'];
-
-                $_SESSION['username'] = $emailPassword['name'];
-                $_SESSION['email'] = $emailPassword['email'];
-                // $_SESSION['video'] = $emailPassword['video'];
-
-                echo $_SESSION['email'];
-
-                echo $_SESSION['username'];
-
-                $passwordDecode = password_verify($CheckPassword,$loginPassword);
-
-                if($passwordDecode){
-                        ?>
-                            <script>
-
-                                alert("redirecting");
-
-                                location.replace("homepage.php");
-                            
-                            </script>
-
-                        <?php
-                    
-                }else{
-
-                    ?>
-
-                        <script>
-
-                        alert("Incorrect password");
-
-                        </script>
-
-
-                    <?php
-                }
-            }
-            
-            else{
-
-                ?>
-
-                        <script>
-
-                        alert("Email doesn't exists,TRY TO SIGN UP.");
-
-                        </script>
-
-
-                    <?php
-
-            }
-        }
-
-    ?>
-
-    
-    <div class="container">
-
+        <div class="left">
+            <img id="image" src="images/authentication.svg" alt="welcome image">
+            <h5 class="text">DON'T HAVE AN ACCOUNT?</h5><br>
+            <h3><a href="signup.php">SIGNUP</a></h3>
+        </div>
+        
         <div class="right">
-
             <?php
+                ?>
                 
-                // echo $_SESSION['nickname'];
-
-                    if(isset($_SESSION['msg'])){
+                <div class="top topp">
+                <?php    
+                if(isset($_SESSION['msg'])){
                         echo $_SESSION['msg'];
                     }
                     else{
-                        echo $_SESSION['msg'] = "Seems you logged out try logging in again";
+                        echo $_SESSION['msg'] = "login";
                     }
                     // echo $_SESSION['message'];
-                
+                ?>
+                </div><br class="hide">
+                <?php
+            
             ?>
-        
-            <form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
+            <div class="top">Why late fill your details</div><br class="show">
+            <div class="center">
+                <form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
+                    <div class="first">
             
-            
-                <input type="text" name="email" id="email" placeholder="email"><br><br>
-                <input type="text" name="password" id="password" placeholder="password"><br><br>
-                <input type="submit" name="submit" value="LOGIN">
+                        <input type="text" name="email" id="email" placeholder="email"><br class="hide"><br class="hide"><br>
+                        <input type="text" name="password" id="password" placeholder="password"><br class="hide">
+                    </div>
 
-            </form>
-        
+            </div>
+                    <div class="bottom">
+                        <input type="submit" name="submit" value="LOGIN">
+                    </div>
         </div>
+                </form>
 
     </div>
 
 </body>
 </html>
+
+
+
+<?php
+    
+    if(isset($_POST['submit'])){
+            
+        $email = mysqli_real_escape_string($con,$_POST['email']);
+        $CheckPassword  = mysqli_real_escape_string($con,$_POST['password']);
+    
+        // $email = $_POST['email'];
+        // $CheckPassword = $_POST['password'];
+
+        if($email == "" || $CheckPassword == ""){
+            ?>
+            <script>
+                alert("Enter the requried fields");
+            </script>
+            <?php
+        }
+        else{
+            // $email = $_POST['email'];
+            // $CheckPassword = $_POST['password'];
+
+            $checkEmailQuery = "SELECT * FROM signup WHERE email = '$email' and status ='active'";
+            
+            $querycheck = mysqli_query($con,$checkEmailQuery);
+
+
+            if(!empty($querycheck)){
+
+
+                // if ($result = mysqli_query($con,$checkEmailQuery) && mysqli_num_rows($result) > 0) {
+                    // there are results in $result
+                // } else {
+                //     // no results
+                // }
+                // if($querycheck = mysqli_query($con,$checkEmailQuery) &&mysqli_num_rows($querycheck)>0 ){
+
+                echo $checkEmailQuery."<br>";
+
+                $loginDetails = mysqli_fetch_assoc($querycheck);
+
+                $userLoginPassword = $loginDetails['password'];
+
+                $_SESSION['username'] = $loginDetails['name'];
+                $_SESSION['email'] = $loginDetails['email'];
+                $_SESSION['nickname'] = $loginDetails['nickname'];
+
+                echo "<br>";
+                echo $_SESSION['username']."<br>".$_SESSION['email']."<br>".$_SESSION['nickname']."<br>";
+
+                $passwordCheck = password_verify($CheckPassword,$userLoginPassword);
+
+                if($passwordCheck){
+                    ?>
+
+                        <script>
+
+                            location.replace("homepage.php");
+
+                        </script>
+
+                    <?php
+                }
+                else{
+
+                    ?>
+
+                    <script>
+                    
+                            alert ("Incorrect Password");
+                    </script>
+
+                    <?php
+                }
+            }
+            else{
+                ?>
+
+                    <script>
+
+                        alert("Email doesn't exists, Try TO Sign Up");
+                    </script>
+
+                <?php
+            }
+        }
+
+    }
+
+    ?>
+
--- signup.php
+++ signup.php
@@ -9,57 +9,56 @@
 <head>
     <title>Document</title>
 
-    <?php include 'css/style.php' ?>
-    <?php include 'scripts/script.php'?>
+    <?php include 'links.php'?>
+    <?php include 'scripts/validation.php'?>
     <?php include 'connection.php' ?>
+    <?php include 'css/signupCSS.php'?>
     
 
 </head>
 <body>
 
-    <div class="container">
+    <div class="signup">
+    <!-- <div class="container"> -->
         <div class="left">
-            <img id="image" src="images/welcome.png" alt="welcome image">
-            <h2 class="hide">WELCOME</h2>
-            <h3 class="hide">Please Enter Details Carefully</h3>
-            <h5>Don't have an account?</h5>
-            <h3><a id="btn" href="login.php">LOGIN</a></h3>
+            
+            <!-- <h2 class="hide">WELCOME</h2><br> -->
+            <!-- <h3 class="hide">Please Enter Details Carefully</h3><br> -->
+            <img id="image" src="images/Girl.svg" alt="welcome image">
+            <h5>HAVE AN ACCOUNT?</h5><br><br class="hide"><br>
+            <h3><a id="top" href="login.php">LOGIN</a></h3><br>
             <h1 class="apply"></h1>
         </div>
         <div class="right">
-            <div class="top">REGISTER</div>
+            <div class="top">Why late fill your details</div><br>
             <div class="center">
                 
                 <form  id="myForm" method="post">
-                        <div class="logo">
-                            <input class="textfield" type="text" name="name" id="name" placeholder=" Enter your Name" >
-                            <input  class="textfield" type="text" name="nickname" id="nikename" placeholder="What do you want us to call you?">
+                    <div class="first">
+                        <input class="textfield" type="text" name="name" id="name" placeholder=" Enter your Name" ><br class="hide"><br class="hide">
+                        <input  class="textfield" type="text" name="nickname" id="nikename" placeholder="Cute name to aall?">
                             
-                        </div> <br class="hide">
-                        
-                        <div class="logo">
-                        <input class="textfield" type="tel" name="mobile" id="phone" placeholder=" Phone Number" pattern="[0-9]{3}[0-9]{2}[0-9]{3}[0-9]{2}" required>
+                    </div>
+                    <div class="first">
+                        <input class="textfield" type="tel" name="mobile" id="phone" placeholder=" Phone Number" pattern="[0-9]{3}[0-9]{2}[0-9]{3}[0-9]{2}" required><br class="hide"><br class="hide">
                         <input  class="textfield" type="email" name="email-id" id="email" placeholder=" Email Id">
-                            
-                        </div> <br class="hide">
-
-                        <div class="logo">
-                            <input class="textfield" type="password" name="password" id="password" onkeyup='check();' placeholder="create password" required>
-                            <input  class="textfield" type="password" name="confirm_password" id="confirm_password" placeholder="confirm password" onkeyup='check();'>
-                        </div>
-                    
-                        <span id='message'></span><br class="hide">
-
-                        <div class="logo register">       
-                            <input type="submit" name='submit' value="Register" onclick="myfunction()">                           
-                        </div>
-                </form><br>
+                    </div>
+                    <div class="first">
+                        <input class="textfield" type="password" name="password" id="password" onkeyup='check();' placeholder="create password" required><br class="hide"><br class="hide">
+                        <input  class="textfield" type="password" name="confirm_password" id="confirm_password" placeholder="confirm password" onkeyup='check();'>
+                    </div>
+                    <span id='message'></span>
+            </div><br class="hide"><br class="hide">
+                <div class="top">
+                <input type="submit" name='submit' value="Register" onclick="myfunction()">                           
+                </div>    
+            </form><br>
                 
-                
-            </div>
-            
+        </div>    
+        
         </div>
 
+    </div>
     </div>
     
 </body>
@@ -91,7 +90,7 @@
 
             $_SESSION['email'] = $email;
 
-            $_SESSION['username'] = $emailPassword['name'];
+            $_SESSION['username'] = $name;
 
 
 
@@ -103,26 +102,54 @@
 
             // $password_decrypt = password_verify($pass,$password_encrypt);
 
-            $emailquery = "SELECT * FROM members where email = '$email'";
+            $emailqueryx = "SELECT * FROM members where email = '$email' and status = 'inactive'";
 
-            $emailx = mysqli_query($con,$emailquery);
+            $emailx = mysqli_query($con,$emailqueryx);
 
-            $emailcount = mysqli_num_rows($emailx);
+            // $emailcount = mysqli_num_rows($emailx);
 
-            if($emailcount>0){
-            ?>
-
+            if ($result = mysqli_query($con,$emailqueryx) && mysqli_num_rows($result) > 0) {
+                // there are results in $result
+                $_SESSION['msg'] = "Seems that Acticavation link is sent already Try activating account :)";
+                ?>
                 <script>
-                    alert("Seems that you have an account Try logging in :)");
-                </script>
 
-            <?php    
+                    alert("redirecting");
+
+                    location.replace("login.php");
+                    </script>
+                <?php
+
+                
+            }
+
+            
+            $emailquery = "SELECT * FROM members where email = '$email' and status = 'active'";
+
+            // $emailxx = mysqli_query($con,$emailquery);
+
+            // $emailcountt = mysqli_num_rows($emailxx);
+
+
+            if ($emailxx = mysqli_query($con,$emailquery) && mysqli_num_rows($emailxx) > 0) {
+            
+                    $_SESSION['msg'] = "Seems that you have an account Try logging in :)";
+                   ?>
+                    <script>
+
+                        alert("redirecting");
+
+                        location.replace("login.php");
+
+                    </script>
+                   <?php
+  
             }
             else{
 
                 
 
-                $query = "INSERT INTO members (name, nickname, phone, email, password, token, status) 
+                $query = "INSERT INTO signup (name, nickname, phone, email, password, token, status) 
                 VALUES ('$name','$nickname','$mobile','$email','$password_encrypt','$token','inactive')";
 
                 $res = mysqli_query($con,$query);
@@ -133,7 +160,7 @@
                     $to = $email;
                     $subject = "Activation Link";
                     $txt = "Hello  {$nickname} Click here to activate your account 
-                    http://localhost:8080/okay/project/activation.php?token=".$token; 
+                    http://localhost:8080/rtcamp/activation.php?token=".$token; 
                     
                     $headers = "From: shulomithi07@gmail.com";
 
@@ -158,7 +185,6 @@
                         // echo "successfull".$to; 
                     }
                     else{
-
                         echo "not";
                     }
 
