cd45078fb67ddfc11cc164e19f9d0d0f8658105c
shulomithi07 <62322842+shulomithi07@users.noreply.github.com>
2021-06-06 13:34:58 +0530

    added comments

--- connection.php
+++ connection.php
@@ -1,25 +1,19 @@
 <?php
 
+
+    // Details of the databse
     $username = "root";
     $password = "";
     $server = 'localhost';
     $database = 'project';
 
-
+    // Connecting to the databse
     $con = mysqli_connect($server,$username,$password,$database);
 
-    if($con){
-        // echo "Connection established";
-        ?>
-        <script>
-            // alert("Connection Established");
-        </script>
-
-        <?php
-    }
-    else{
-        // echo "NOT CONNECTED";
+    if(!$con){
         die('NOT CONNECTED'. mysqli_connect_error());
+        
     }
+    
 
 ?>
