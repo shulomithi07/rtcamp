b8c56687b77f5190f8317e129ea346468b53e70a
shulomithi07 <62322842+shulomithi07@users.noreply.github.com>
2021-06-06 19:48:38 +0530

    Deletes converted files from convert page& logout.

--- logout.php
+++ logout.php
@@ -2,9 +2,25 @@
 
 session_start();
 
+
+
+// specifying directory
+$mydir = 'uploads/convert';
+ 
+//scanning files in a given directory in descending order
+$myfiles = scandir($mydir, 1);
+ 
+//displaying the files in the directory
+print_r($myfiles);
+$n = sizeof($myfiles);
+for($i=0;$i<$n-2;$i++){
+    // echo $myfiles[$i]."<br>";
+    $path = "uploads/convert/$myfiles[$i]";
+    unlink($path);
+}
+
 session_destroy();
-
-
+// redirecting to login page
 header('location:login.php')
 
 
