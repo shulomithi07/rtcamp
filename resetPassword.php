5a68beaa5bbb71cc379a419c12ad199ef3ab14ae
shulomithi07 <62322842+shulomithi07@users.noreply.github.com>
2021-06-11 13:44:33 +0530

    A small syntax error is corrected

--- resetPassword.php
+++ resetPassword.php
@@ -304,6 +304,7 @@ if(isset($_POST['submit'])){
             <?php
         }
     // If query didn't execute this message will be shown in the reset password page
+    }
     else{
         $_SESSION['msg'] = "Something happened please try again!";
         ?>
